c4f11f2f0b4c62565f3f2177a2f5b3fa755c4b7a
EBER <ebertoespinosa@gmail.com>
2024-07-26 10:33:56 -0400

    config1

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex-col">
-      <div className='container mx-auto text-2xl'>Hello</div>
-    </main>
-  );
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+
+  return <div>Hello MDC 27 - {data?.user.email} </div>;
 }
